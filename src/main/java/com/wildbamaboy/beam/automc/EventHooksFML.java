1f73628ae1cf7efc1496f369dffc1a7e04e787dd
WildBamaBoy <wildbamaboy@gmail.com>
2016-01-07 14:02:06 -0600

    Cherry pick f5a1c5f

--- src/main/java/com/wildbamaboy/beam/automc/EventHooksFML.java
+++ src/main/java/com/wildbamaboy/beam/automc/EventHooksFML.java
@@ -1,33 +1,37 @@
 package com.wildbamaboy.beam.automc;
 
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
 public class EventHooksFML 
 {
 	private final int INTERVAL_THRESHOLD = 5;
 	private int counter = 0;
-	
+
 	@SubscribeEvent
 	public void clientTickEventHandler(ClientTickEvent event)
 	{
-		if (counter >= INTERVAL_THRESHOLD)
+		if (AutoMC.instance.getRobot() != null)
 		{
-			counter = 0;
-			
-			EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-			
-			if (player != null && player.isCollidedHorizontally)
+			if (counter >= INTERVAL_THRESHOLD)
 			{
-				player.jump();
+				counter = 0;
+
+				EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+
+				//Make sure we're on the ground before jumping in order to prevent double jumps.
+				if (player != null && player.isCollidedHorizontally && player.onGround)
+				{
+					player.jump();
+				}
 			}
-		}
-		
-		else
-		{
-			counter++;
+
+			else
+			{
+				counter++;
+			}
 		}
 	}
 }
